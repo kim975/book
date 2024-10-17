24e26b2fe7e8bfe9a794dc3f19c48f71126c93a7
kim <scilo200@gmail.com>
2024-10-18 04:25:36 +0900

    feat : 상품 등록, 수정, 검색 기능 개발

---
+++ order/src/main/java/com/zerobase/order/order/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.zerobase.order.order.service;
+
+import com.zerobase.order.order.domain.model.BookProductRelationEntity;
+import com.zerobase.order.order.domain.model.ProductEntity;
+import com.zerobase.order.order.domain.repository.ProductReader;
+import com.zerobase.order.order.domain.repository.ProductStore;
+import com.zerobase.order.order.service.ProductCommand.ModifyProduct;
+import com.zerobase.order.order.service.ProductInfo.Main;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductReader productReader;
+    private final ProductStore productStore;
+
+    public void registerProduct(ProductCommand.RegisterProduct command) {
+
+        ProductEntity product = productStore.store(command.toEntity());
+
+        productStore.store(BookProductRelationEntity.builder()
+            .productId(product.getId())
+            .bookId(command.getBookId())
+            .build()
+        );
+
+    }
+
+    @Transactional
+    public void modifyProduct(ModifyProduct command) {
+
+        ProductEntity product = productReader.getProductByProductId(command.getProductId());
+
+        product.setName(command.getName());
+        product.setPrice(command.getPrice());
+        product.setQuantity(command.getQuantity());
+
+    }
+
+    public Main getProduct(Long productId) {
+        return Main.from(productReader.getProductByProductId(productId));
+    }
+}
